66624e8b3e96c357471bf94b4617e1634041a9f5
Damith Weerawardana <damith@bistecglobal.com>
2023-05-29 16:02:52 +0530

    Fixes

---
+++ BackendEvaluation.API/Program.cs
@@ -0,0 +1,27 @@
+using BackendEvaluation.Infrastructure;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddInfrastructure();
+    //.AddCore();
+
+builder.Host.UseSerilog((context, Configuration) => Configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+app.UseHttpsRedirection();
+
+app.Run();
---
+++ BackendEvaluation.Domain/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+namespace BackendEvaluation.Domain;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        return services;
+    }
+}
