23d52ae009ff6c1c99897bacca780483534cd933
Damith Weerawardana <damith@bistecglobal.com>
2023-05-31 14:50:22 +0530

    Add Identity Server

---
+++ BackendEvaluation.Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BackendEvaluation.Identity.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
